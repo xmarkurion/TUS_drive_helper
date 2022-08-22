c2397d8a6973bbf40c44bd9f6d9b621b2834bb14
unknown <xmarkurion@gmail.com>
2022-08-22 12:39:24 +0100

    names for class

---
+++ src/main/java/com/markurion/tus_java_ondrive_helper/ZipMaster.java
@@ -0,0 +1,11 @@
+package com.markurion.tus_java_ondrive_helper;
+
+public class ZipMaster {
+    private String inFolder;
+    private String outFolder;
+
+    public ZipMaster(String inputfolder,String outputFolder){
+        this.inFolder = inputfolder;
+        this.outFolder = outputFolder;
+    }
+}
