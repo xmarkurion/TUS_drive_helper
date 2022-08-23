3c14e64bbff9696e978d9bf1c53a3d22fb5e4f66
unknown <xmarkurion@gmail.com>
2022-08-23 03:02:52 +0100

    icon

--- src/main/java/com/markurion/tus_java_ondrive_helper/Main.java
+++ src/main/java/com/markurion/tus_java_ondrive_helper/Main.java
@@ -3,6 +3,7 @@ package com.markurion.tus_java_ondrive_helper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +11,13 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image(String.valueOf(getClass().getResource("images/icon.png")));
+        stage.getIcons().add(icon);
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("TUS Ondrive helper");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
